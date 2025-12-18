94b281c7da2377ac30dc6a6dbd451c6d03802dc2
Marie <marie-l.plekat@hotmail.com>
2025-12-18 12:14:05 +0100

    Added Localized Text to Text Components in Scene

--- Fairy-Business/Assets/Scripts/UI/Menu/PauseMenu.cs
+++ Fairy-Business/Assets/Scripts/UI/Menu/PauseMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using Locations;
 using UI.Menu.BaseMenu;
 using UnityEngine;
@@ -8,14 +9,29 @@ namespace UI.Menu
     public class PauseMenu : MenuElement
     {
         [SerializeField] private Button returnToStartScreen;
+        [SerializeField] private Button languageButton;
         
 
         private void Awake()
         {
-            returnToStartScreen.onClick.AddListener(ReturnToStartScreen);
+            returnToStartScreen.onClick.AddListener(OnReturnToStartScreenButtonClicked);
+            languageButton.onClick.AddListener(OnLanguageButtonClicked);
         }
-        
-        private void ReturnToStartScreen()
+
+        private void OnDestroy()
+        {
+            returnToStartScreen.onClick.RemoveAllListeners();
+            languageButton.onClick.RemoveAllListeners();
+        }
+
+        public void OnLanguageButtonClicked()
+        {
+            string languageCode = "de";
+            Localizer.instance.SetLanguage(languageCode);
+            AppUser.SaveOption("currentLanguageCode", languageCode);
+        }
+
+        private void OnReturnToStartScreenButtonClicked()
         {
             CloseMenu();
             LocationManager.instance.ResetGameLocations();
